93b9e5bccd033b1523390237bad8dfa2e5114321
Esalencia <esalenciar@gmail.com>
2022-09-07 15:46:30 -0700

    simple shell

--- builtin_utils1.c
+++ builtin_utils1.c
@@ -1,97 +1,107 @@
 #include "shell.h"
 
 /**
- * _myexit - exits the shell
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- *  Return: exits with a given exit status
- *         (0) if info.argv[0] != "exit"
+ * _setenv - sets environment variable
+ * @env: the current environment
+ * Return: 0 on success.
  */
-int _myexit(info_t *info)
+int _setenv(char ***env)
 {
-	int exitcheck;
-
-	if (info->argv[1])  /* If there is an exit arguement */
-	{
-		exitcheck = _erratoi(info->argv[1]);
-		if (exitcheck == -1)
-		{
-			info->status = 2;
-			print_error(info, "Illegal number: ");
-			_eputs(info->argv[1]);
-			_eputchar('\n');
-			return (1);
-		}
-		info->err_num = _erratoi(info->argv[1]);
-		return (-2);
-	}
-	info->err_num = -1;
-	return (-2);
+char *env_var, *env_val;
+env_var = strtok(NULL, " \n");
+if (env_var == NULL)
+{
+p_error("missing argument\n");
+return (1);
 }
-
-/**
- * _mycd - changes the current directory of the process
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- *  Return: Always 0
- */
-int _mycd(info_t *info)
+env_val = strtok(NULL, " \n");
+if (env_val == NULL)
 {
-	char *s, *dir, buffer[1024];
-	int chdir_ret;
-
-	s = getcwd(buffer, 1024);
-	if (!s)
-		_puts("TODO: >>getcwd failure emsg here<<\n");
-	if (!info->argv[1])
-	{
-		dir = _getenv(info, "HOME=");
-		if (!dir)
-			chdir_ret = /* TODO: what should this be? */
-				chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
-		else
-			chdir_ret = chdir(dir);
-	}
-	else if (_strcmp(info->argv[1], "-") == 0)
-	{
-		if (!_getenv(info, "OLDPWD="))
-		{
-			_puts(s);
-			_putchar('\n');
-			return (1);
-		}
-		_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
-		chdir_ret = /* TODO: what should this be? */
-			chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
-	}
-	else
-		chdir_ret = chdir(info->argv[1]);
-	if (chdir_ret == -1)
-	{
-		print_error(info, "can't cd to ");
-		_eputs(info->argv[1]), _eputchar('\n');
-	}
-	else
-	{
-		_setenv(info, "OLDPWD", _getenv(info, "PWD="));
-		_setenv(info, "PWD", getcwd(buffer, 1024));
-	}
-	return (0);
+p_error("missing value\n");
+return (1);
 }
-
+return (_setenv_func(env, env_var, env_val));
+}
 /**
- * _myhelp - changes the current directory of the process
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- *  Return: Always 0
+ * _setenv_func - helps _setenv
+ * @env: the current environment
+ * @env_var: enviroment variable name
+ * @env_val: enviroment variable value
+ * Return: 0 on success
  */
-int _myhelp(info_t *info)
+int _setenv_func(char ***env, char *env_var, char *env_val)
 {
-	char **arg_array;
-
-	arg_array = info->argv;
-	_puts("help call works. Function not yet implemented \n");
-	if (0)
-		_puts(*arg_array); /* temp att_unused workaround */
-	return (0);
+char **new_env;
+int i;
+for (i = 0; (*env)[i] != NULL; i++)
+{
+if (_cstrcmp((*env)[i], env_var) == 0)
+{
+free((*env)[i]);
+(*env)[i] = new_path_str(env_var, env_val);
+return (0);
+}
+}
+new_env = grow_env(*env, i);
+if (env == NULL)
+{
+perror("grow_env");
+return (1);
+}
+free(*env);
+*env = new_env;
+(*env)[i] = new_path_str(env_var, env_val);
+print((*env)[i]);
+putch('\n');
+(*env)[++i] = NULL;
+return (0);
+}
+/**
+ * grow_env - grow env by one, copy all the present key-value pairs
+ * @env: the current environment
+ * @env_size: the size of the current environment, not including '\0'.
+ * Return: the new environment, NULL on memory error.
+ */
+char **grow_env(char **env, int env_size)
+{
+int i;
+char **new_env;
+/* grow environment by one + NULL pointer */
+new_env = malloc(sizeof(char **) * (env_size + 2));
+if (new_env == NULL)
+{
+perror("malloc");
+return (NULL);
+}
+/* copy all the pointers to strings to new environment */
+for (i = 0; i < env_size; i++)
+new_env[i] = env[i];
+/* return new environment */
+return (new_env);
+}
+/**
+ * new_path_str - new string with format "variable" + '=' + "value"
+ * @env_var: environment variable name
+ * @env_val: environment variable value
+ * Return: the new PATH string.
+ */
+char *new_path_str(char *env_var, char *env_val)
+{
+int env_var_len, env_val_len, path_len = 0;
+char *path_str;
+env_var_len = _strlen(env_var);
+env_val_len = _strlen(env_val);
+/* '=' + '\0' --> 2 additional chars */
+path_len = env_var_len + env_val_len + 2;
+path_str = malloc(sizeof(char) * (path_len));
+if (path_str == NULL)
+{
+perror("malloc");
+exit(EXIT_FAILURE);
+}
+_memset(path_str, '\0', path_len);
+path_str = _strcat(path_str, env_var);
+path_str = _strcat(path_str, "=");
+path_str = _strcat(path_str, env_val);
+return (path_str);
 }
