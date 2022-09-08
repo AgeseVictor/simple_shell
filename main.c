de0c34eb316dde465993e8e1fb737f5722b5bb08
Esalencia <esalenciar@gmail.com>
2022-09-08 05:34:20 -0700

    collaoration simple shell

--- main.c
+++ main.c
@@ -1,52 +1,44 @@
 #include "shell.h"
-/**
- * main - Entry point for program execution
- * @argc: length of arg array
- * @argv: arguments array
- * @env: current env variables passed to child processes
- * Return: 0 Success
- */
 
-int main(int argc __attribute__ ((unused)), char *argv[], char **env)
+/**
+ * main - entry point
+ * @ac: arg count
+ * @av: arg vector
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int ac, char **av)
 {
-FILE *istream = 0;
-char *lineptr = NULL, *cmd = NULL;
-size_t llen = 0;
-ssize_t input = 0;
-istream = stdin;
-env = current_env(env);
-if (signal(SIGINT, interrupt_handler) == SIG_IGN)
-signal(SIGINT, SIG_IGN);
-while (1)
-{
-if (isinteractive())
-prompt1();
-input = getline(&lineptr, &llen, istream);
-if (istream == NULL)
-{
-perror(argv[0]);                  /* failed to open input stream? STDIN */
-return (EXIT_FAILURE);
-}
-if (input == EOF)      /* terminate shell with 'Ctrl + D' */
-{
-free(lineptr);
-free_env(env);
-if (errno == 0)
-return (EXIT_SUCCESS);
-perror(argv[0]);
-return (EXIT_FAILURE);
-}
-ESC_comments(lineptr);
-cmd = strtok(lineptr, " \n");
-if (cmd != NULL && exec_builtins(cmd, lineptr, &env))
-{ /* exit, env, help, setenv,.. builtin cmds get executed */
-if (cmd_executions(cmd, lineptr, &env))
-{ /* forks child process gets cmd path checks waits program execution */
-free_env(env);
-perror(argv[0]);
-return (EXIT_FAILURE);
-}
-}
-}
-return (EXIT_SUCCESS);
+	info_t info[] = { INFO_INIT };
+	int fd = 2;
+
+	asm ("mov %1, %0\n\t"
+		"add $3, %0"
+		: "=r" (fd)
+		: "r" (fd));
+
+	if (ac == 2)
+	{
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
+		{
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
+		}
+		info->readfd = fd;
+	}
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
+	return (EXIT_SUCCESS);
 }
